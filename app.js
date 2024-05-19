05bdcc3c81892f69d501ff2f26c438df1bd7ae5e
CT Nava <ctnava97@gmail.com>
2024-05-19 11:34:20 -0700

    fix cors & separated token endpoint

--- app.js
+++ app.js
@@ -81,68 +81,6 @@ require('./lib/routes.js').addRoutes(app, evm);
 // 		}
 // 	);
 
-// app
-// 	.route('/api/token/:tokenId')
-// 	.post(async (req, res) => {
-// 		const { user_address, network, metadata } = req.body;
-// 		try {
-// 			// const newToken = new Token(metadata);
-// 			// await newToken.save();
-// 			const abtContract = getABT(network);
-// 			await (await abtContract.quickMint()).wait();
-// 			let numTokens = parseInt(await abtContract.numTokens());
-// 			await (
-// 				await abtContract.transferFrom(
-// 					evm.wallet.address,
-// 					user_address,
-// 					numTokens
-// 				)
-// 			).wait();
-// 			res.sendStatus(200).send(numTokens);
-// 		} catch (error) {
-// 			res.status(400).send(error);
-// 		}
-// 	})
-// 	.get(async (req, res) => {
-// 		try {
-// 			const token = await Token.findOne({ _id: req.params.tokenId });
-// 			if (!token) {
-// 				return res.status(404).send('Token not found');
-// 			}
-// 			res.send(token);
-// 		} catch (error) {
-// 			res.status(500).send(error);
-// 		}
-// 	})
-// 	.put(async (req, res) => {
-// 		try {
-// 			const updates = req.body;
-// 			const options = { new: true };
-// 			const token = await Token.findByIdAndUpdate(
-// 				req.params.tokenId,
-// 				updates,
-// 				options
-// 			);
-// 			if (!token) {
-// 				return res.status(404).send('Token not found');
-// 			}
-// 			res.send(token);
-// 		} catch (error) {
-// 			res.status(400).send(error);
-// 		}
-// 	})
-// 	.delete(async (req, res) => {
-// 		try {
-// 			const token = await Token.findByIdAndDelete(req.params.tokenId);
-// 			if (!token) {
-// 				return res.status(404).send('Token not found');
-// 			}
-// 			res.send({ message: 'Token deleted successfully' });
-// 		} catch (error) {
-// 			res.status(500).send(error);
-// 		}
-// 	});
-
 // app.post('/upload', upload.none(), (req, res) => {
 // 	const { ext, chunk, chunkIndex, totalChunks } = req.body;
 // 	const dataChunk = decomposeFile(chunk, chunkIndex, totalChunks);
